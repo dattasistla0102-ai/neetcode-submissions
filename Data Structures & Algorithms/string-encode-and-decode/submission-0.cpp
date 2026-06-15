1d5607271cb363a4fe1fede1dd7bd34a4eadfba9
dattasistla0102-ai <dattasistla0102@gmail.com>
2026-06-15 22:25:35 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string encoded="";
+        for(string s:strs)
+        {
+            encoded=encoded+to_string(s.size())+"#"+s;
+        }
+        return encoded;
+        
+    }
+
+    vector<string> decode(string s) {
+        vector<string> ans;
+        int i=0;
+        while(i<s.size())
+        {
+            int j=i;
+            while(s[j]!='#')
+            {
+                j++;
+            }
+            int length=stoi(s.substr(i,j-i));
+            string str=s.substr(j+1,length);
+            ans.push_back(str);
+            i=j+length+1;
+        }
+        return ans;
+        
+    }
+};
