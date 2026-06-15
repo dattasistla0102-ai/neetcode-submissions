c3aa941d3b8ced1b03ab31a8b20fc41dd582f42f
dattasistla0102-ai <dattasistla0102@gmail.com>
2026-06-15 20:15:21 +0530

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res(2);
+        unordered_map<int,int>m;
+        for(int i=0;i<nums.size();i++)
+        {
+            int digit=target-nums[i];
+            if(m.count(digit))
+            {
+                res[0]=m[digit];
+                res[1]=i;
+            }
+            m[nums[i]]=i;
+        }
+        return res;
+    }
+};
